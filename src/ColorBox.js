bec49496cae73c9da515fb339d2dc0c2cfc35725
Janan Akkad <jananakkad@hotmail.co.uk>
2023-05-04 16:38:57 +0100

    complete

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -16,5 +16,19 @@ export default class ColorBox extends Component {
     )
   }
 
+  // // or
+  // render (props) {
+  //   if (this.props.opacity >= 0.2) {
+  //     console.log(this.props.opacity)
+  //     return (
+  //       <div className="color-box" style={{opacity: this.props.opacity}}>
+  //         <ColorBox opacity={this.props.opacity - 0.1} />
+  //       </div>
+  //     )
+  //   } else {
+  //     return null
+  //   }
+  // }
+
 }
 
